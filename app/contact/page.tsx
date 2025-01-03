d82f4c32af9412cd62eaeca83347ee1ca6a43f82
Fathah KA <48355244+fathah@users.noreply.github.com>
2025-01-03 10:26:43 +0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,10 +13,14 @@ const ContactUs = () => {
                     {/* --------------- CONTACT DETAILS ---------------- */}
                     <div className="text-lg">
                         <img src="/images/logo.png" className="h-12 mt-10 mb-3" alt="" />
-                        <p>Al Nahda, Sharja, UAE</p>
-                        <p>Po Box. 97378</p>
+                        <p>Jasim Almidfa Building - 205</p>
+                        <p>Kuwait Square, Maysaloon </p>
+                        <p>Al Sharq, Sharjah, UAE</p>
 
                         <section className="mt-4">
+                             <h6 className="flex items-center gap-x-3 mb-2"><FaSquarePhone className="text-3xl" />
+                                <div className="text-lg font-bold font-sans">065 481 813</div>
+                            </h6>
                             <h6 className="flex items-center gap-x-3 mb-2"><FaSquarePhone className="text-3xl" />
                                 <div className="text-lg font-bold font-sans">+971 509 769 862</div>
                             </h6>
@@ -47,7 +51,7 @@ const ContactUs = () => {
 
                     <div>
                     <iframe 
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27428.31592737886!2d55.335444305850395!3d25.303820729267724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5c769ceaebc5%3A0x87ef0a2e9e93dfdc!2sAl%20Nahda%20-%20Sharjah!5e1!3m2!1sen!2sae!4v1723873300221!5m2!1sen!2sae"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3605.418035808438!2d55.4040342!3d25.3573025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f591ce61c3d57%3A0x8f1e0b27b82c520d!2sFajar%20Manpower!5e0!3m2!1sen!2sae!4v1735885560788!5m2!1sen!2sae"
                     width="100%" height="100%" 
                     className="rounded-2xl"
                      allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
